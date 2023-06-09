61347eb568c59746a5761b9514e14ea6f1ce6e12
Breno Farias <brenodasfarias2001@gmail.com>
2023-06-09 16:22:23 -0300

    Deploy in Azure

---
+++ CRUD CSHARP WEB/CRUD CSHARP WEB/Models/BancoDeDados.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_CSHARP_WEB.Models
+{
+    public class BancoDeDados : DbContext
+    {
+        public DbSet<Cliente> Clientes { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: @"Server=(localdb)\mssqllocaldb;Database= CRUD CSHARP WEB;Integrated Security=True");
+        }
+    }
+}
---
+++ CRUD CSHARP WEB/CRUD CSHARP WEB/Models/Cliente.cs
@@ -0,0 +1,10 @@
+﻿namespace CRUD_CSHARP_WEB.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public string Telefone { get; set; }
+    }
+}
